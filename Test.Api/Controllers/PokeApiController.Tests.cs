7db42da956faedfd849a5313ada0df2c5cdf0297
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-18 09:31:07 -0300

    teste get by name

--- Test.Api/Controllers/PokeApiController.Tests.cs
+++ Test.Api/Controllers/PokeApiController.Tests.cs
@@ -17,13 +17,31 @@ namespace Test.Api.Controllers
             var client = application.CreateClient();
 
             // act
-            var response = await client.GetAsync(BASE_PATH + "/" + number);
+            var response = await client.GetAsync(BASE_PATH + "?number=" + number);
 
             var actual = await response.DeserializeContentAsync<External.PokeApi.Models.Pokemon>();
 
             // assert
             Assert.AreEqual(number, actual.Id);
             Assert.AreEqual(expected, actual.Name);
+        }
+
+        [Test]
+        [TestCase(151, "mew")]
+        public async Task Should_Get_Pokemon_By_Name(int expected, string name)
+        {
+            // arrange
+            await using var application = new WebAppFactory("Development");
+            var client = application.CreateClient();
+
+            // act
+            var response = await client.GetAsync(BASE_PATH + "?name=" + name);
+
+            var actual = await response.DeserializeContentAsync<External.PokeApi.Models.Pokemon>();
+
+            // assert
+            Assert.AreEqual(name, actual.Name);
+            Assert.AreEqual(expected, actual.Id);
         }
     }
 }
